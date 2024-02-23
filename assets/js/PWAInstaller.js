18f8bb0dae6a02b6793e3c4e67cd84de4d35712e
faustinopsy <rodrigohipnose@gmail.com>
2024-02-22 21:02:52 -0300

    adicionado  cache/offline/PWA/Servicework

---
+++ assets/js/PWAInstaller.js
@@ -0,0 +1,133 @@
+class PWAInstaller {
+    constructor() {
+        this.deferredPrompt = null;
+        this.setupButton = null;
+        this.lido = false;
+    }
+
+    initialize() {
+        this.setupListeners();
+        this.checkReadState();
+        this.registerServiceWorker();
+    }
+
+    checkReadState() {
+        this.lido = localStorage.getItem('lido');
+        if (!this.lido) {
+            this.mostrarModalNaoFechavel();
+        }
+    }
+
+    mostrarModalNaoFechavel() {
+        localStorage.setItem('lido', true);
+        const modal = document.createElement('div');
+        modal.style.position = 'fixed';
+        modal.style.top = '0';
+        modal.style.left = '0';
+        modal.style.width = '100%';
+        modal.style.height = '100%';
+        modal.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        modal.style.backdropFilter = 'blur(15px)';
+        modal.style.display = 'flex';
+        modal.style.justifyContent = 'center';
+        modal.style.alignItems = 'center';
+        modal.style.zIndex = '9000';
+
+        const modalContent = document.createElement('div');
+        modalContent.style.backgroundColor = '#fff';
+        modalContent.style.padding = '20px';
+        modalContent.style.borderRadius = '5px';
+
+        const closeButton = document.createElement('button');
+        closeButton.textContent = 'Fechar';
+        closeButton.style.position = 'absolute';
+        closeButton.style.top = '10px';
+        closeButton.style.right = '10px';
+        closeButton.style.padding = '5px 10px';
+        closeButton.style.background = 'red';
+        closeButton.style.color = 'white';
+        closeButton.style.border = 'none';
+        closeButton.style.borderRadius = '5px';
+        closeButton.style.cursor = 'pointer';
+
+        closeButton.onclick = function () {
+            document.body.removeChild(modal);
+        };
+
+        modalContent.innerHTML += `
+            <h2>🚀 Seja bem vindo!</h2>
+            <p>Olá, esse app é um modelo SPA.</p>
+            <p> Este é um modelo para ser expandido, como forma de estudo!</p>
+            <p>Espero que goste, feito por um professor para alunos.</p> 
+        `;
+
+        modal.appendChild(modalContent);
+        modalContent.appendChild(closeButton);
+        document.body.appendChild(modal);
+    }
+
+    setupListeners() {
+        window.addEventListener('DOMContentLoaded', () => {
+            this.checkReadState();
+        });
+
+        window.addEventListener('beforeinstallprompt', (e) => {
+            e.preventDefault();
+            this.deferredPrompt = e;
+
+            const installModal = document.createElement('div');
+            installModal.setAttribute('id', 'installModal');
+            installModal.style.zIndex = '97000';
+            installModal.style.top = '100px';
+            installModal.innerHTML = `
+                <div class="w3-panel w3-pale-green">
+                    <h2>Instalar Aplicativo</h2>
+                    <p>Quer instalar para uma experiência completa?</p>
+                    <button id="installBtn" class="w3-button w3-white w3-border w3-border-blue w3-round">📱 Instalar</button>
+                    <button id="cancelInstallBtn" class="w3-button w3-white w3-border w3-border-red w3-round">❌ Cancelar</button>
+                </div>
+            `;
+            document.body.appendChild(installModal);
+
+            installModal.style.display = 'block';
+            document.getElementById('installBtn').addEventListener('click', () => {
+                this.deferredPrompt.prompt();
+                this.deferredPrompt.userChoice.then((choiceResult) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('Usuário aceitou a instalação do PWA');
+                    } else {
+                        console.log('Usuário recusou a instalação do PWA');
+                    }
+                    this.deferredPrompt = null;
+                    installModal.style.display = 'none';
+                });
+            });
+
+            document.getElementById('cancelInstallBtn').addEventListener('click', () => {
+                installModal.style.display = 'none';
+            });
+        });
+
+        window.addEventListener('appinstalled', (evt) => {
+            console.log("appinstalled fired", evt);
+        });
+    }
+
+    registerServiceWorker() {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker
+                .register('./sw.js')
+                .then(serviceWorker => {
+                    console.log('Rodando serviço: ' + serviceWorker);
+                })
+                .catch(error => {
+                    console.log('Error registering the Service Worker: ' + error);
+                });
+        }
+    }
+}
+
+const pwaInstaller = new PWAInstaller();
+window.addEventListener('load', () => {
+    pwaInstaller.initialize();
+});
---
+++ sw.js
@@ -0,0 +1,92 @@
+const CACHE_NAME = 'app-cache-v2';
+const STATIC_ASSETS = [
+    '/',
+    '/manifest.json',
+    '/index.html',
+    '/favicon.ico',
+    '/assets/img/imagem1.png',
+    '/assets/img/imagem2.png',
+    '/assets/img/imagem3.png',
+    '/assets/img/img0.jpg',
+    '/assets/img/img1.jpg',
+    '/assets/img/img2.jpg',
+    '/assets/img/img3.jpg',
+    '/assets/img/logo.png',
+    '/assets/css/style.css',
+    '/assets/css/w3.css',
+    '/assets/js/App.js',
+    '/assets/js/router.js',
+    '/assets/js/PWAInstaller.js',
+    '/assets/js/componentes/card.js',
+    '/assets/js/componentes/formContato.js',
+    '/assets/js/componentes/navbar.js',
+    '/assets/js/componentes/slides.js',
+    '/assets/js/componentes/slides/slideImages.js',
+    '/assets/js/componentes/slides/slideControls.js',
+    '/assets/js/api/FetchData.js',
+    '/assets/json/postagens.json',
+    '/assets/js/paginas/home.js',
+    '/assets/js/paginas/sobre.js',
+    '/assets/js/paginas/contato.js',
+];
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME]; 
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName); 
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+      caches.open(CACHE_NAME).then(async cache => {
+          try {
+              await cache.addAll(STATIC_ASSETS);
+              const jsonURL = './assets/json/postagens.json';
+              const jsonResponse = await fetch(jsonURL);
+              if (jsonResponse.ok) {
+                await cache.put(jsonURL, jsonResponse);
+            }
+          } catch (error) {
+              console.error("Erro durante o cache.addAll: ", error);
+              throw error;
+          }
+      })
+  );
+});
+  
+self.addEventListener('fetch', event => {
+  event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+          if (cachedResponse) {
+              return cachedResponse;
+          }
+
+          return fetch(event.request).then(networkResponse => {
+              if (networkResponse.ok) {
+                  return caches.open(CACHE_NAME).then(cache => {
+                      cache.put(event.request, networkResponse.clone());
+                      return networkResponse;
+                  });
+              }
+              return caches.match('offline.html'); 
+          }).catch(() => {
+              return caches.match('offline.html'); 
+          });
+      }).catch(() => {
+          return caches.match('offline.html'); 
+      })
+  );
+}); 
+ 
+  
+  
+  
+  
