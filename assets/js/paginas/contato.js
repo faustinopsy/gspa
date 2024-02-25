e31128017580a998225f2203dbe1d4edd869f8a0
faustinopsy <rodrigohipnose@gmail.com>
2024-02-25 16:31:15 -0300

    modified:   assets/js/paginas/contato.js

--- assets/js/paginas/contato.js
+++ assets/js/paginas/contato.js
@@ -7,8 +7,21 @@ class Contato {
     render(i18nService) {
         this.i18nService = i18nService;
         return `
-        <h1>${this.i18nService.t('contact')}</h1>
-        ${this.formulario.render(this.i18nService)}
+        <div class="w3-cell-row">
+            <div class="w3-container w3-cell w3-mobile">
+                <h1>${this.i18nService.t('contact')}</h1>
+                ${this.formulario.render(this.i18nService)}
+            </div>
+            <div class="w3-container  w3-cell w3-mobile">
+                <h1>${this.i18nService.t('contact')}</h1>
+                <div class="w3-card w3-center w3-leftbar">
+                <img src="./assets/img/imagem2.webp" style="width:50%" alt="Alps">
+                <div class="w3-container w3-center">
+                <p>The Italian / Austrian Alps</p>
+                </div>
+            </div>
+            </div>
+        </div>      
          `
     }
     afterRender(){
