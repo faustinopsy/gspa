f34fda40dc8a2bf3b67cb3f679f91c6e586284c2
faustinopsy <rodrigohipnose@gmail.com>
2024-02-29 15:15:42 -0300

    modified:   assets/css/style.css 	modified:   assets/js/componentes/floatingButton.js 	modified:   assets/js/componentes/slides/slideImages.js

--- assets/js/componentes/floatingButton.js
+++ assets/js/componentes/floatingButton.js
@@ -3,10 +3,8 @@ class FloatingButton {
         this.i18nService = i18nService;
         this.navegar = navegar;
         this.buttonElement = document.createElement('button');
-        //this.buttonElement.textContent = this.i18nService.t('open');
         this.buttonElement.classList.add('floating-button');
     }
-
     render() {
         document.body.appendChild(this.buttonElement);
         this.buttonElement.addEventListener('click', () => this.navegar('#config'));
